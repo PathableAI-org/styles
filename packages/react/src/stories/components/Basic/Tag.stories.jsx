89f914ca939e9c25a23646e77261c382def85294
Jake Shilling <shilling.jake@gmail.com>
2026-07-18 16:44:00 -0400

    fix(react): remove invalid tag aria label

--- packages/react/src/stories/components/Basic/Tag.stories.jsx
+++ packages/react/src/stories/components/Basic/Tag.stories.jsx
@@ -64,7 +64,7 @@ export const CustomAttributes = {
 }
 
 export const EmptyContent = {
-  render: () => <Tag aria-label="Empty tag example" />,
+  render: () => <Tag />,
 }
 
 export const UnsupportedSizeFallback = {
