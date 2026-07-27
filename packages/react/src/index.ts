7825817451ae3bef52e50b88f5c1f037361cb591
Frank <Franklin.buono33@gmail.com>
2026-07-27 11:13:56 -0400

    feat(react): add EmptyState wrapper

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -7,6 +7,11 @@ import '@pathable/styles/dist/styles.css'
 export { Button } from './components/Button/Button.js'
 export { ButtonGroup } from './components/button-group/ButtonGroup.js'
 export { Card } from './components/Card/Card.js'
+export { EmptyState } from './components/EmptyState/EmptyState.js'
+export type {
+  EmptyStateProps,
+  EmptyStateVariant,
+} from './components/EmptyState/EmptyState.js'
 export { List } from './components/List/List.js'
 export { Table } from './components/Table/Table.js'
 export { Textarea } from './components/Textarea/Textarea.js'
