b0a4c3d6712d8e5beeec25e86a47bc8932ede1c3
Frank <Franklin.buono33@gmail.com>
2026-07-28 10:56:46 -0400

    feat(react): add Checkbox wrapper

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -14,6 +14,8 @@ export type {
 export { Button } from './components/Button/Button.js'
 export { ButtonGroup } from './components/button-group/ButtonGroup.js'
 export { Card } from './components/Card/Card.js'
+export { Checkbox } from './components/Checkbox/Checkbox.js'
+export type { CheckboxProps } from './components/Checkbox/Checkbox.js'
 export { List } from './components/List/List.js'
 export { Table } from './components/Table/Table.js'
 export { Textarea } from './components/Textarea/Textarea.js'
